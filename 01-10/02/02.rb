612d6a044465c5f55d9c9134de29f91ce89a4fdf
Aoyama Taro <a11111111@aoyama.jp>
2021-12-12 11:53:48 +0900

    [finish] 02

--- 01-10/02/02.rb
+++ 01-10/02/02.rb
@@ -3,7 +3,7 @@
 
 =end
 
-
+# AC回答 ref済み
 N = gets.to_i
 if N.odd?
   puts ""
@@ -11,24 +11,14 @@ if N.odd?
 end
 list = []
 
-["(",")"].repeated_permutation(N).each do |bits|
+["(",")"].repeated_permutation(N).each do |bits| #O(2^N)
   next unless bits.count("(") == bits.count(")") #(,)の数が等しい必要がある O(N)
 
-  cnt_l = 0
-  cnt_r = 0
-  is_all_ok = bits.all? do |b|
-    b == "(" ? cnt_l += 1 : cnt_r += 1
-    cnt_l >= cnt_r
+  open = close = 0
+  is_all_ok = bits.all? do |b| # O(N)
+    b == "(" ? open += 1 : close += 1
+    open >= close
   end
-  if is_all_ok
-    list << bits.join
-  end
-
-  # bits.each do |b|
-  #   b == "(" ? cnt_l += 1 : cnt_r += 1
-  #   next if cnt_l < cnt_r
-  # end
-  
+  list << bits.join if is_all_ok
 end
-
 puts list
